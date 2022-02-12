5d38193e83d938fbf94d801c8c98212cca4e0ad8
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-02-12 20:15:28 -0300

    finished kinova

--- projetos/projeto-2/kinova/kinova_mdl.m
+++ projetos/projeto-2/kinova/kinova_mdl.m
@@ -26,7 +26,7 @@ for switch_xd = 0:2
     K1 = Ks(1,switch_xd+1);
     K2 = Ks(2,switch_xd+1);
     
-    T = 10;
+    T = 20;
     
     sim('kinova_kinctrl1',T);
     sim('kinova_kinctrl2',T);
@@ -34,38 +34,48 @@ for switch_xd = 0:2
     %% Plot PIJ
     %%% Control
     figure;
+    subplot(2,1,1)
     plot(u1);
     legend({'$u_1$','$u_2$','$u_3$','$u_4$'},'Interpreter','latex');
     xlabel('$t$','Interpreter','latex');
     ylabel('$u(t)$','Interpreter','latex')
     title("Sinal de controle para o controlador PIJ e trajetória " + int2str(switch_xd+1));
-    saveas(gcf,"imgs/u_PIJ_xd"+int2str(switch_xd+1));
+    
+    %%% Error
+    subplot(2,1,2)
+    plot(e1);
+    legend({'$e_x$','$e_y$','$e_z$'},'Interpreter','latex');
+    xlabel('$t$','Interpreter','latex');
+    ylabel('$e(t)$','Interpreter','latex')
+    title("Erro para o controlador PIJ e trajetória " + int2str(switch_xd+1));
+    saveas(gcf,"imgs/ue_PIJ_xd"+int2str(switch_xd+1)+".png");
+    
     %%% Position
     figure;
     plot(x1.data,z1.data);
     xlabel('$x(t)$','Interpreter','latex');
     ylabel('$z(t)$','Interpreter','latex')
     title("Posição para o controlador PIJ e trajetória " + int2str(switch_xd+1));
-    saveas(gcf,"imgs/x_PIJ_xd"+int2str(switch_xd+1));
-
-    %%% Error
-    figure;
-    plot(e1);
-    legend({'$e_1$','$e_2$','$e_3$'},'Interpreter','latex');
-    xlabel('$t$','Interpreter','latex');
-    ylabel('$e(t)$','Interpreter','latex')
-    title("Posição para o controlador PIJ e trajetória " + int2str(switch_xd+1));
-    saveas(gcf,"imgs/e_PIJ_xd"+int2str(switch_xd+1));
+    saveas(gcf,"imgs/x_PIJ_xd"+int2str(switch_xd+1)+".png");
 
     %% Plot TJ
-    %%% Control
     figure;
+    %%% Control
+    subplot(2,1,1)
     plot(u2);
     legend({'$u_1$','$u_2$','$u_3$','$u_4$'},'Interpreter','latex');
     xlabel('$t$','Interpreter','latex');
     ylabel('$u(t)$','Interpreter','latex')
     title("Sinal de controle para o controlador TJ e trajetória " + int2str(switch_xd+1));
-    saveas(gcf,"imgs/u_TJ_xd"+int2str(switch_xd+1));
+    
+    %%% Error
+    subplot(2,1,2)
+    plot(e2);
+    legend({'$e_x$','$e_y$','$e_z$'},'Interpreter','latex');
+    xlabel('$t$','Interpreter','latex');
+    ylabel('$e(t)$','Interpreter','latex')
+    title("Erro para o controlador TJ e trajetória " + int2str(switch_xd+1));
+    saveas(gcf,"imgs/ue_TJ_xd"+int2str(switch_xd+1)+".png");
 
     %%% Position
     figure;
@@ -73,14 +83,7 @@ for switch_xd = 0:2
     xlabel('$x(t)$','Interpreter','latex');
     ylabel('$z(t)$','Interpreter','latex')
     title("Posição para o controlador TJ e trajetória " + int2str(switch_xd+1));
-    saveas(gcf,"imgs/x_TJ_xd"+int2str(switch_xd+1));
+    saveas(gcf,"imgs/x_TJ_xd"+int2str(switch_xd+1)+".png");
 
-    %%% Error
-    figure;
-    plot(e2);
-    legend({'$e_1$','$e_2$','$e_3$'},'Interpreter','latex');
-    xlabel('$t$','Interpreter','latex');
-    ylabel('$e(t)$','Interpreter','latex')
-    title("Posição para o controlador TJ e trajetória " + int2str(switch_xd+1));
-    saveas(gcf,"imgs/e_TJ_xd"+int2str(switch_xd+1));
+
 end
